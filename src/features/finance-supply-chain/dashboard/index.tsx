d8484e8e49cb536c97abbdbc2c2651092db002b2
adityashekharsharma <adityasharmasfa@gmail.com>
2026-07-14 18:02:13 +0530

    [Finance Dashboard] - feature : added all modules financial data in single dashboard

--- src/features/finance-supply-chain/dashboard/index.tsx
+++ src/features/finance-supply-chain/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import DashboardPage from './pages/Dashboard';
+import KpiDetailPage from './pages/KpiDetailPage';
 
 export default function Dashboard() {
   return (
     <Routes>
       <Route index element={<DashboardPage />} />
+      <Route path=":kpiId" element={<KpiDetailPage />} />
       <Route path="*" element={<DashboardPage />} />
     </Routes>
   );
